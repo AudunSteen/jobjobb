a970d0da16031e93e9d7001fd71505f94cbd2273
audunsteen <audunste@uia.no>
2023-12-05 14:33:30 +0100

    dashboard

--- JobApplicationSystem/www/site/dashboard.php
+++ JobApplicationSystem/www/site/dashboard.php
@@ -1,70 +1,68 @@
 <?php
+
+//starter session for å kunne gi ulike rettigheter i systemet.
 session_start();
 
-include 'inc/session.php'; //Sjekker om bruker er logget inn
-include 'inc/header.php'; //Navbar og rettigheter
-
-// Funksjon for å lagre opplastede filer på serveren
-function saveUploadedFile($fileInputName, $destinationFolder)
-{
-    $targetDir = $destinationFolder;
-    $targetFile = $targetDir . basename($_FILES[$fileInputName]['name']);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
-
-    // Sjekk om filen allerede eksisterer
-    if (file_exists($targetFile)) {
-        echo "Beklager, filen eksisterer allerede.";
-        $uploadOk = 0;
-    }
-
-    // Sjekk filtypen (du kan legge til flere godkjente filtyper etter behov)
-    if ($imageFileType != "pdf" && $imageFileType != "doc" && $imageFileType != "docx") {
-        echo "Beklager, kun PDF, DOC og DOCX-filer er tillatt.";
-        $uploadOk = 0;
-    }
-
-
-
-    // Sjekk om $uploadOk er satt til 0 av en feil
-    if ($uploadOk == 0) {
-        echo "Beklager, filen ble ikke lastet opp.";
-    } else {
-        // Last opp filen hvis alt er i orden
-        if (move_uploaded_file($_FILES[$fileInputName]['tmp_name'], $targetFile)) {
-            echo "Filen " . htmlspecialchars(basename($_FILES[$fileInputName]['name'])) . " har blitt lastet opp.";
-        } else {
-            echo "Beklager, det oppstod en feil under opplastingen av filen.";
-        }
-    }
+// Sjekk om brukeren er logget inn
+if (!isset($_SESSION['username'])) {
+    header("Location: login.php"); // Send brukeren tilbake til innloggingssiden hvis ikke logget inn
+    exit();
 }
 
-// Behandle skjemaet for filopplasting
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Lagre firmaets profilbilde for arbeidsgiver
-    if ($_SESSION['userRole'] == 'arbeidsgiver' && isset($_FILES['companyProfileImage'])) {
-        saveUploadedFile('companyProfileImage', 'uploads/company_profiles/');
-    }
+include 'inc/header.php';
 
-    // Lagre CV for jobbsøker
-    if ($_SESSION['userRole'] == 'jobbsoker' && isset($_FILES['cvFile'])) {
-        saveUploadedFile('cvFile', 'uploads/jobseeker_cvs/');
-    }
+// Melding til brukeren basert om brukeren er arbeidsgiver eller jobbsoker
+$userTypeMessage = "";
+if ($_SESSION['userType'] === 'arbeidsgiver') {
+    $userTypeMessage = "Du er registrert som arbeidsgiver. Velkommen til vårt jobbsøkesystem! Her kan du publisere ledige stillinger, håndtere søknader og finne passende kandidater for dine jobbannonser.";
+} elseif ($_SESSION['userType'] === 'jobbsoker') {
+    $userTypeMessage = "Du er registrert som jobbsøker. Velkommen til vårt jobbsøkesystem! Her kan du opprette din profil, legge til CV, søke på ledige stillinger, og holde oversikt over dine søknader.";
+} else {
+    $userTypeMessage = "Velkommen til jobbsøkesystemet, " . $_SESSION['username'] . "!";
 }
 ?>
 
-<h1>Velkommen til din dashboard, <?php echo $_SESSION['username']; ?>!</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<!-- Skjema for filopplasting -->
-<form action="" method="post" enctype="multipart/form-data">
-    <?php if ($_SESSION['userRole'] == 'arbeidsgiver') : ?>
-        <label for="companyProfileImage">Last opp profilbilde for firma:</label>
-        <input type="file" name="companyProfileImage" accept=".png, .jpg, .jpeg"><br>
-    <?php elseif ($_SESSION['userRole'] == 'jobbsoker') : ?>
-        <label for="cvFile">Last opp CV:</label>
-        <input type="file" name="cvFile" accept=".pdf, .doc, .docx"><br>
-    <?php endif; ?>
-    <input type="submit" value="Last opp fil">
-</form>
+<head>
+    <title>Velkommen til jobbsøkesystemet</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f4f4f4;
+            margin: 0;
+            padding: 0;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 0 auto;
+            padding: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            border-radius: 8px;
+            margin-top: 20px;
+        }
+
+        h1 {
+            color: #333;
+            margin-top: 0;
+        }
+
+        p {
+            color: #555;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Velkommen til jobbsøkesystemet, <?php echo $_SESSION['username']; ?>!</h1>
+        <p><?php echo $userTypeMessage; ?></p>
+    </div>
+</body>
+
+</html>
 
 <?php include 'inc/footer.php'; ?>
