82761475e746302c12733cb4290506639de2d85f
audunsteen <audunste@uia.no>
2023-12-05 12:09:51 +0100

    brukerinfo på de som har søkt

--- JobApplicationSystem/www/site/sokere.php
+++ JobApplicationSystem/www/site/sokere.php
@@ -3,7 +3,6 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-
 if (!isset($_SESSION['username']) || $_SESSION['userType'] !== 'arbeidsgiver') {
     header("Location: login.php");
     exit();
@@ -25,22 +24,54 @@ if ($conn->connect_error) {
 // Get jobbannonse_id from the URL parameter
 $jobbannonse_id = isset($_GET['jobbannonse_id']) ? $_GET['jobbannonse_id'] : 0;
 
-$sql_select_soknader = "SELECT * FROM soknader WHERE jobbannonse_id = ?";
-$stmt_select_soknader = $conn->prepare($sql_select_soknader);
-$stmt_select_soknader->bind_param("i", $jobbannonse_id);
-$stmt_select_soknader->execute();
-$result_soknader = $stmt_select_soknader->get_result();
+// Hent tittel fra jobbannonser
+$sql_select_tittel = "SELECT tittel FROM jobbannonser WHERE id = ?";
+$stmt_select_tittel = $conn->prepare($sql_select_tittel);
+$stmt_select_tittel->bind_param("i", $jobbannonse_id);
+$stmt_select_tittel->execute();
+$result_tittel = $stmt_select_tittel->get_result();
 
-if ($result_soknader->num_rows > 0) {
-    while ($row = $result_soknader->fetch_assoc()) {
-        echo "Soknad ID: " . $row["soknad_id"] . " - Jobbannonse ID: " . $row["jobbannonse_id"] . " - Jobbsoker ID: " . $row["jobbsoker_id"];
-        echo " - Soknadstekst: " . $row["soknadstekst"] . " - Soknadsdato: " . $row["soknadsdato"] . " - PDF Path: " . $row["pdf_path"] . "<br>";
+if ($result_tittel->num_rows > 0) {
+    $row_tittel = $result_tittel->fetch_assoc();
+    $jobbannonse_tittel = $row_tittel['tittel'];
+
+    echo "<h1>Søkere på stillingen som  $jobbannonse_tittel </h1>";
+
+    // Hent søknader med phoneNumber og email fra brukeren
+    $sql_select_soknader = "SELECT soknader.*, users.username, users.phoneNumber, users.email FROM soknader 
+                            JOIN users ON soknader.jobbsoker_id = users.id 
+                            WHERE soknader.jobbannonse_id = ?";
+    $stmt_select_soknader = $conn->prepare($sql_select_soknader);
+    $stmt_select_soknader->bind_param("i", $jobbannonse_id);
+    $stmt_select_soknader->execute();
+    $result_soknader = $stmt_select_soknader->get_result();
+
+    if ($result_soknader->num_rows > 0) {
+        echo "<table border='1'>";
+        echo "<tr><th>Bruker</th><th>Søknadstekst</th><th>Søknadsdato</th><th>PDF</th><th>PhoneNumber</th><th>Email</th></tr>";
+
+        while ($row = $result_soknader->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td>" . $row["username"] . "</td>";
+            echo "<td>" . $row["soknadstekst"] . "</td>";
+            echo "<td>" . $row["soknadsdato"] . "</td>";
+            echo "<td><a href='" . $row["pdf_path"] . "' target='_blank'>Last ned PDF</a></td>";
+            echo "<td>" . $row["phoneNumber"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "Ingen søknader funnet.";
     }
+
+    $stmt_select_soknader->close();
 } else {
-    echo "Ingen søknader funnet.";
+    echo "Feil: Fant ikke tittel for jobbannonse.";
 }
 
-$stmt_select_soknader->close();
+$stmt_select_tittel->close();
 
 include 'inc/footer.php';
 ?>
